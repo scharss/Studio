d4ae91fe80403082e02bad2224630af7476e9866
Scharss <scharss@gmail.com>
2021-06-11 02:39:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var a = [5,2,4,6,1,3];
+console.log(a);
+var c = [];
+for(var i=0; i<a.length; i++){
+    var b = a[i];
+    c.push(b);
+    for(var j=0; j<c.length; j++){
+        
+    }
+
+}
+console.log(c.sort());
